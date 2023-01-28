4fdb601801ba817b6cc98ead20d9656226d8d28c
kalakood <>
2023-01-28 04:37:19 -0500

    .

--- screen_ext.sh
+++ screen_ext.sh
@@ -1,8 +1,6 @@
 # curl -s https://raw.githubusercontent.com/kalakood/conf/main/screen_ext.sh | bash -s
-echo -n "Hostname:"
-read hostname 
-echo $hostname
 cat << EOF > ~/.screenrc
+
 shelltitle ""
 escape ^ll
 startup_message off
@@ -11,7 +9,7 @@ bind k focus up
 bind j focus down
 bind h focus left
 bind l focus right
-caption always "%{0}<<< \033[93m$hostname\033[0m | %n:%t >>>"
+caption always "%{0}<<< \033[93m$1\033[0m | %n:%t >>>"
 rendition so "="
 EOF
 
